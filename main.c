cd27e0b90b1c55ca55bca8b8827bc1829b16176f
Дьячков Денис Андреевич <is641s13@lwp05.csc.local>
2017-02-13 08:38:54 +0700

    Create body of program

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float getPercentOfInc(int days, float deposit)
+{
+    float percent;
+    return percent;
+}
+
+int inputData(int* days, int* deposit)
+{
+    return 1;
+}
+
+int main()
+{
+    return 0;
+}
