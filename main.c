2cc608f35c78697e781aa3436a80d77f345cc78a
Дьячков Денис Андреевич <is641s13@lwp05.csc.local>
2017-02-13 08:58:58 +0700

    Validate user input

--- main.c
+++ main.c
@@ -3,21 +3,46 @@
 float getPercentOfInc(int days, float deposit)
 {
     float percent;
+    
     return percent;
 }
 
 int inputData(int* days, int* deposit)
 {
     printf("Please input days: ");
-    scanf("%d", days);
+    if (!scanf("%d", days))
+    {
+        printf("Days is incorrect\n");
+        return 0;
+    }
+    else
+       if (*days < 0 || *days > 365)
+       {
+           printf("Out of range days\n");
+           return 0;
+       }
+
     printf("Please input deposit: ");
-    scanf("%d", deposit);
+    if (!scanf("%d", deposit))
+    {
+        printf("Deposit is incorrect\n");
+        return 0;
+    }
+    else
+       if (*deposit < 10000)
+       {
+           printf("Deposit is too small\n");
+           return 0;
+       }
     return 1;
 }
 
 int main()
 {
     int days, deposit;
-    inputData(&days, &deposit);
+    if (!inputData(&days, &deposit)){
+        return 1;    
+    }
+    
     return 0;
 }
