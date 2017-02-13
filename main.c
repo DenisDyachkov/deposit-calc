86a3d671600ce94935619706adb8c2a515b9fe46
Дьячков Денис Андреевич <is641s13@lwp05.csc.local>
2017-02-13 09:25:54 +0700

    Implement income calculation

--- main.c
+++ main.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
 
+const int depDelim = 100000;
+
 float getPercentOfInc(int days, float deposit)
 {
-    float percent;
-    
+    float percent = 1;
+    if(days <= 30) {
+	percent -= 0.1;
+    } else if(days <= 120) {
+        percent += (deposit <= depDelim ? 0.02 : 0.03);
+    } else if(days <= 240) {
+        percent += (deposit <= depDelim ? 0.06 : 0.08);
+    } else if(days <= 365) {
+        percent += (deposit <= depDelim ? 0.12 : 0.15);
+    }
     return percent;
 }
 
-int inputData(int* days, int* deposit)
+int inputData(int *days, int *deposit)
 {
     printf("Please input days: ");
-    if (!scanf("%d", days))
-    {
+    if (!scanf("%d", days)) {
         printf("Days is incorrect\n");
         return 0;
-    }
-    else
-       if (*days < 0 || *days > 365)
-       {
-           printf("Out of range days\n");
-           return 0;
-       }
-
-    printf("Please input deposit: ");
-    if (!scanf("%d", deposit))
-    {
-        printf("Deposit is incorrect\n");
+    } else if (*days < 0 || *days > 365) {
+        printf("Out of range days\n");
         return 0;
     }
-    else
-       if (*deposit < 10000)
-       {
-           printf("Deposit is too small\n");
-           return 0;
-       }
+
+    printf("Please input deposit: ");
+    if (!scanf("%d", deposit)) {
+        printf("Deposit is incorrect\n");
+        return 0;
+    } else if (*deposit < 10000) {
+        printf("Deposit is too small\n");
+        return 0;
+    }
     return 1;
 }
 
 int main()
 {
     int days, deposit;
-    if (!inputData(&days, &deposit)){
+    if (!inputData(&days, &deposit)) {
         return 1;    
     }
     
+    float income = deposit * getPercentOfInc(days, deposit);
+    printf("Income: %.2f\n", income);  
+   
     return 0;
 }
