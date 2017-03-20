666b54eee8d4aed072560073bbb06e894e5c8c55
Дьячков Денис Андреевич <is641s13@awp06.csc.local>
2017-03-20 09:23:46 +0700

    Update project struct

---
+++ src/deposit.h
@@ -0,0 +1,8 @@
+#ifndef _DEPOSIT_H_
+#define _DEPOSIT_H_
+
+float getPercentOfInc(int days, float deposit);
+int   inputData(int *days, int *deposit);
+float getIncome(int days, float deposit);
+
+#endif
